4872b1be373bbfff332a4352a6e696934774b7e6
Saathvik-Sheerla <140387813+Saathvik-Sheerla@users.noreply.github.com>
2023-09-09 21:24:03 +0530

    app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def local_css(file_name):
 	with open(file_name) as f:
 		st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-local_css("style/style.css.txt")
+local_css("style.css.txt")
 
 
 
