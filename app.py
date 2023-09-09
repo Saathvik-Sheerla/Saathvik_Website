31156e9fcaa2cbd74faecb82af32b1728d3c2683
Saathvik-Sheerla <140387813+Saathvik-Sheerla@users.noreply.github.com>
2023-09-09 20:29:04 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from PIL import Image
+
+
+st.title("Saathvik's Resume")
+#st.image('')
+
+
+
+
+contact_form = """
+<form action="https://formsubmit.co/shravyarinky29@gmail.com" method="POST">
+<input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="your name" required>
+     <input type="email" name="email" placeholder="your em@il" required>
+     <textarea name="message" placeholder="your message here"></textarea>
+     <button type="submit">Send</button>
+</form>
+"""
+
+st.markdown(contact_form , unsafe_allow_html=True)
+
+def local_css(file_name):
+	with open(file_name) as f:
+		st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+local_css("style/style.css.txt")
+
+
+
+st_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                </style>
+                """
+
+st.markdown(st_style , unsafe_allow_html=True)
